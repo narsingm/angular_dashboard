cbd2093e374a9affe31175ab9a0a77406801302d
Jatin-synsoft <maheshwarinarsing@gmail.com>
2026-07-01 17:37:07 +0530

    Add users page with table data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './layouts/dashboard/dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
 import { authGuard } from './guards/auth.guard';
+import { UsersComponent } from './pages/users/users.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -15,7 +16,7 @@ export const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
       // Other dummy routes can be added here, they can just point to home for now or be left empty
-      { path: 'users', component: HomeComponent },
+      { path: 'users', component: UsersComponent },
       { path: 'reports', component: HomeComponent },
       { path: 'settings', component: HomeComponent },
       { path: 'profile', component: HomeComponent }
